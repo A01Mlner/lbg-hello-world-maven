41ac192015cb9eefc6448f2fb54f1a95f8cc8d96
A01Mlner <milneradam0@gmail.com>
2024-11-04 11:53:55 +0000

    EDIT: Added my name to the java outputs

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("A01Milner you are the goat"));
         System.out.println(sayGoodbye());
     }
 
